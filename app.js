6b611afd6e03ff23e5173dd3f55d90267b4dd085
Yash Singhal <singhalyash8080@gmail.com>
2020-09-12 00:52:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const express=require('express')
+const app=express();
+app.use(express.static('./'))
+app.get('/',(req,res)=>res.send('./index.html'))
+app.listen(process.env.PORT||3000)
---
+++ js/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+$(window).on('load', function() {
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+(function($) {
+
+	
+	$('.nav-switch').on('click', function(event) {
+		$('.main-menu').slideToggle(400);
+		event.preventDefault();
+	});
+
+
+	
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	
+	$('.hero-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		mouseDrag: false,
+		animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+		items: 1,
+		autoplay: true
+	});
+	var dot = $('.hero-slider .owl-dot');
+	dot.each(function() {
+		var index = $(this).index() + 1;
+		if(index < 10){
+			$(this).html('0').append(index);
+			$(this).append('<span>.</span>');
+		}else{
+			$(this).html(index);
+			$(this).append('<span>.</span>');
+		}
+	});
+
+
+	
+	$('.news-ticker').marquee({
+	    duration: 10000,
+	  
+	    delayBeforeStart: 0,
+	    direction: 'left',
+	    duplicated: true
+	});
+
+})(jQuery);
